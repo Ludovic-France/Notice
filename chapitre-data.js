fd67a566671d7591b5e99c531a12a0025ca7056d
Ludovic-France <65298222+Ludovic-France@users.noreply.github.com>
2025-06-23 15:46:08 +0200

    Update chapitre-data.js

--- chapitre-data.js
+++ chapitre-data.js
@@ -2,12 +2,11 @@ const ChapitreData = [
     { "id": "introduction", "H1": "Introduction" },
     { "id": "description", "H1": "Description Générale" },
     { "id": "safety", "H1": "Consignes de Sécurité" },
-    { "id": "operation", "H1": "Fonctionnement", "subchapters": [
+    { "id": "operation", "H1": "Fonctionnement", "H2_items": [
         { "id": "op_manual", "H2": "Mode Manuel" },
         { "id": "op_auto", "H2": "Mode Automatique" }
     ]},
     { "id": "maintenance", "H1": "Maintenance" },
     { "id": "troubleshooting", "H1": "Dépannage" },
     { "id": "annexes", "H1": "Annexes" }
-
 ];
