cac60f82a40f8c882c2107c1fd17064ea8ee9700
Ludovic-France <65298222+Ludovic-France@users.noreply.github.com>
2025-06-26 11:26:06 +0200

    Add files via upload

---
+++ constante.js
@@ -0,0 +1,30 @@
+const Laser = [ 
+	"chapitre": "Substance et émission", 
+	"H4": "Laser",
+	"contenuHTML": "<div id='laser'>Présence de laser dans la machine, le risque etc.</div>
+	<div class='drop-target'></div>"
+	];
+const Fumee = [ 
+	"chapitre": "Substance et émission", 
+	"H4": "Fumée",
+	"contenuHTML": "<div id='fumee'>De la fumée est émise lors de la soudure. Une aspiration dédiée etc.</div>
+	<div class='drop-target'></div>"
+	];
+const Poussiere = [ 
+	"chapitre": "Substance et émission", 
+	"H4": "Fumée",
+	"contenuHTML": "<div id='poussiere'>De la poussière est émise lors de la soudure. Une aspiration dédiée etc.</div>
+	<div class='drop-target'></div>"
+	];
+const UV = [ 
+	"chapitre": "Substance et émission", 
+	"H4": "Fumée",
+	"contenuHTML": "<div id='uv'>De la fumée est émise lors de la soudure. Une aspiration dédiée etc.</div>
+	<div class='drop-target'></div>"
+	];
+const IR = [ 
+	"chapitre": "Substance et émission", 
+	"H4": "Fumée",
+	"contenuHTML": "<div id='ir'>De la fumée est émise lors de la soudure. Une aspiration dédiée etc.</div>
+	<div class='drop-target'></div>"
+	];
